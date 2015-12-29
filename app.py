44073bb4191ed485b6c995d7082d31c6f8002512
Joe Singleton <joesingo@gmail.com>
2015-12-29 23:47:44 +0000

    Use couchdb to store IOUs as JSON documents. Have home page show a list of all IOUs

--- app.py
+++ app.py
@@ -1,25 +1,75 @@
+import json
 from flask import Flask, session, redirect, render_template, request
+import requests
 
+# Make sure usenames do not contain ';;;'!
 PASSWORDS = {
     "joe": "pass"
 }
 
+BASE_DB_URL = "http://localhost:5984/"
+DB_URL = BASE_DB_URL + "iou/"
+
 app = Flask(__name__)
+
+def create_iou(a, b):
+    """
+    Create a new IOU doc for persons a and b and save in DB. Return True if
+    successful, False if not
+    """
+    doc = {}
+    doc["people"] = [a, b]
+    doc["transactions"] = []
+    doc["currency"] = "GBP"
+
+    res = requests.get(BASE_DB_URL + "_uuids")
+    uuid = json.loads(res.content)["uuids"][0]
+
+    res = requests.put(DB_URL + uuid, data=json.dumps(doc))
+    content = json.loads(res.content)
+
+    return "error" not in content
+
 
 def is_logged_in():
     return "username" in session
 
+
 @app.route("/")
 def home():
     """
-    Home page - show list of IOUs
+    Show a list of all IOUs for this user and the associated transactions for
+    each
     """
-
     if not is_logged_in():
-        return redirect("/login")
+        return redirect("/login/")
 
-    else:
-        return "hey"
+    res = requests.get(DB_URL + "_design/iou/_view/getious/?key=\"{}\"" \
+                                .format(session["username"]))
+
+    content = json.loads(res.content)
+
+    if "error" in content:
+        abort(500)
+
+    data = {}
+
+    for row in content["rows"]:
+        # Get the username of the other person
+        names = row["value"]["people"]
+        names.remove(session["username"])
+        person = names[0]
+
+        data[person] = row["value"]
+
+        # Work out the amount owed to the user
+        data[person]["owed"] = 0
+        for t in row["value"]["transactions"]:
+            sign = 1 if t["lender"] == session["username"] else -1
+            data[person]["owed"] += sign * t["amount"]
+
+    return render_template("list.html", data=data)
+
 
 @app.route("/login/")
 def login_form(login_error=False):
