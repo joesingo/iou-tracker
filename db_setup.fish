eeeb38fec63d19dabd8b732d70d58b52db858528
Joe Singleton <joesingo@gmail.com>
2016-04-20 14:27:10 +0100

    Added file to automate db setup

---
+++ db_setup.fish
@@ -0,0 +1,8 @@
+#!/bin/fish
+set db_url "http://localhost:5984/iou"
+
+# Create database
+curl -X PUT $db_url
+
+# Create design doc
+curl -H "Content-Type: application/json" -X POST -d @couchdb.json $db_url
